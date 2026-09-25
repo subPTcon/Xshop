60101efcedcb7d5f6360ce191ffc0bee4c7aa680
Michael_Yuen <michaelcst2003@gmail.com>
2026-09-25 21:59:24 +0800

    product-service项目初始化，pojo实现

---
+++ product-service/src/main/java/org/michael/xshop/pojo/Product.java
@@ -0,0 +1,61 @@
+package org.michael.xshop.pojo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@TableName("t_product")
+public class Product {
+
+    /**
+     * 商品ID
+     */
+    @TableId(type = IdType.AUTO)
+    private Long id;
+
+    /**
+     * 所属类目ID
+     */
+    private Long categoryId;
+
+    /**
+     * 商品标题
+     */
+    private String title;
+
+    /**
+     * 商品副标题
+     */
+    private String subTitle;
+
+    /**
+     * 商品主图
+     */
+    private String mainImage;
+
+    /**
+     * 商品详情HTML
+     */
+    private String detailHtml;
+
+    /**
+     * 商品状态
+     * 1: 上架
+     * 0: 下架
+     */
+    private Integer status;
+
+    /**
+     * 销量
+     *
+     * 冗余字段，异步更新
+     */
+    private Integer saleCount;
+
+    private LocalDateTime createTime;
+    private LocalDateTime updateTime;
+}
---
+++ product-service/src/main/java/org/michael/xshop/pojo/Sku.java
@@ -0,0 +1,57 @@
+package org.michael.xshop.pojo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@TableName("t_sku")
+public class Sku {
+
+    @TableId(type = IdType.AUTO)
+    private Long id;
+
+    /**
+     * 所属商品ID
+     */
+    private Long productId;
+
+    /**
+     * SKU编码
+     * 全局唯一
+     */
+    private String skuCode;
+
+    /**
+     * 商品规格JSON
+     *
+     * 例如：
+     * {"颜色":"黑色", "内存":"256G"}
+     */
+    private String specJson;
+
+    /**
+     * SKU价格
+     */
+    private BigDecimal price;
+
+    /**
+     * SKU图片
+     */
+    private String image;
+
+    /**
+     * SKU状态
+     * 1: 正常
+     * 0: 禁用
+     */
+    private Integer status;
+
+    private LocalDateTime createTime;
+
+
+}
